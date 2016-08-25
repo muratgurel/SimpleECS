008fe9af117667d85d81149646b93621471bf57a
Murat Gurel <murat@gram.gs>
2016-08-25 15:29:43 +0300

    Adds entity predicate & its tests

---
+++ SimpleECS/EntityPredicate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleECS
+{
+	public class EntityPredicate : IPredicate<Entity>
+	{
+		internal readonly HashSet<Type> includedTypes = new HashSet<Type>();
+		internal readonly HashSet<Type> exludedTypes = new HashSet<Type>();
+
+		public static EntityPredicate New
+		{
+			get
+			{
+				return new EntityPredicate();
+			}
+		}
+
+		public bool Matches(Entity obj)
+		{
+			foreach (var type in includedTypes)
+			{
+				if (!obj.HasComponent(type))
+				{
+					return false;
+				}
+			}
+
+			foreach (var type in exludedTypes)
+			{
+				if (obj.HasComponent(type))
+				{
+					return false;
+				}
+			}
+
+			return true;
+		}
+	}
+
+	public static class EntityPredicateExtensions
+	{
+		public static EntityPredicate Include<T>(this EntityPredicate predicate)
+		{
+			predicate.includedTypes.Add(typeof(T));
+			return predicate;
+		}
+
+		public static EntityPredicate Include<T, U>(this EntityPredicate predicate)
+		{
+			predicate.includedTypes.UnionWith(new Type[2] { typeof(T), typeof(U) });
+			return predicate;
+		}
+
+		public static EntityPredicate Include<T, U, V>(this EntityPredicate predicate)
+		{
+			predicate.includedTypes.UnionWith(new Type[3] { typeof(T), typeof(U), typeof(V) });
+			return predicate;
+		}
+
+		public static EntityPredicate Exclude<T>(this EntityPredicate predicate)
+		{
+			predicate.exludedTypes.Add(typeof(T));
+			return predicate;
+		}
+
+		public static EntityPredicate Exclude<T, U>(this EntityPredicate predicate)
+		{
+			predicate.exludedTypes.UnionWith(new Type[2] { typeof(T), typeof(U) });
+			return predicate;
+		}
+
+		public static EntityPredicate Exclude<T, U, V>(this EntityPredicate predicate)
+		{
+			predicate.exludedTypes.UnionWith(new Type[3] { typeof(T), typeof(U), typeof(V) });
+			return predicate;
+		}
+	}
+}
+
---
+++ SimpleECSTests/EntityPredicateTests.cs
@@ -0,0 +1,86 @@
+﻿using NUnit.Framework;
+
+namespace SimpleECS.Test
+{
+	[TestFixture]
+	public class EntityPredicateTests
+	{
+		private World world;
+		private EntityPredicate predicate;
+
+		[SetUp]
+		public void SetUp()
+		{
+			world = new World();
+			predicate = EntityPredicate.New;
+		}
+
+		[Test]
+		public void ShouldMatchAllEntitiesWithEmptyPredicate()
+		{
+			Entity emptyEntity = world.CreateEntity();
+
+			Entity entityWithEmptyComponentOne = world.CreateEntity();
+			entityWithEmptyComponentOne.AddComponent<EmptyComponentOne>();
+
+			Entity entityWithAllThreeComponents = world.CreateEntity();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentOne>();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentTwo>();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentThree>();
+
+			Assert.IsTrue(predicate.Matches(emptyEntity));
+			Assert.IsTrue(predicate.Matches(entityWithEmptyComponentOne));
+			Assert.IsTrue(predicate.Matches(entityWithAllThreeComponents));
+		}
+
+		[Test]
+		public void ShouldFilterEntitiesWithExcludedTypes()
+		{
+			predicate.Exclude<EmptyComponentTwo>();
+
+			Entity entityWithEmptyComponentTwo = world.CreateEntity();
+			entityWithEmptyComponentTwo.AddComponent<EmptyComponentTwo>();
+
+			Entity entityWithAllThreeComponents = world.CreateEntity();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentOne>();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentTwo>();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentThree>();
+
+			Assert.IsFalse(predicate.Matches(entityWithEmptyComponentTwo));
+			Assert.IsFalse(predicate.Matches(entityWithAllThreeComponents));
+		}
+
+		[Test]
+		public void ShouldReturnEntitiesWithIncludedTypes()
+		{
+			predicate.Include<EmptyComponentTwo>();
+
+			Entity entityWithEmptyComponentTwo = world.CreateEntity();
+			entityWithEmptyComponentTwo.AddComponent<EmptyComponentTwo>();
+
+			Entity entityWithAllThreeComponents = world.CreateEntity();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentOne>();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentTwo>();
+			entityWithAllThreeComponents.AddComponent<EmptyComponentThree>();
+
+			Assert.IsTrue(predicate.Matches(entityWithEmptyComponentTwo));
+			Assert.IsTrue(predicate.Matches(entityWithAllThreeComponents));
+		}
+
+		[Test]
+		public void ShouldBeChainable()
+		{
+			predicate.Include<EmptyComponentOne>().Include<EmptyComponentTwo>().Exclude<EmptyComponentThree>();
+
+			Entity entityWithOneAndTwoComponents = world.CreateEntity();
+			entityWithOneAndTwoComponents.AddComponent<EmptyComponentOne>();
+			entityWithOneAndTwoComponents.AddComponent<EmptyComponentTwo>();
+
+			Assert.IsTrue(predicate.Matches(entityWithOneAndTwoComponents));
+		}
+
+		private class EmptyComponentOne : IComponent { }
+		private class EmptyComponentTwo : IComponent { }
+		private class EmptyComponentThree : IComponent { }
+	}
+}
