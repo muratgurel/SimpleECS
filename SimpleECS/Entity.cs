ac0214c2574358c2344e8ea54ff75af581429dd4
Murat Gurel <murat@gram.gs>
2016-08-25 13:29:09 +0300

    Adds component count property to entity

--- SimpleECS/Entity.cs
+++ SimpleECS/Entity.cs
@@ -5,6 +5,14 @@ namespace SimpleECS
 {
 	public class Entity
 	{
+		public int componentCount
+		{
+			get
+			{
+				return components.Count;
+			}
+		}
+
 		private Dictionary<Type, IComponent> components = new Dictionary<Type, IComponent>();
 	}
 }
