09d473d317b1bd9787efc779dd8f36205c86605c
Murat Gurel <murat@gram.gs>
2016-08-25 13:20:25 +0300

    Adds create & destroy entity methods to world

--- SimpleECS/World.cs
+++ SimpleECS/World.cs
@@ -5,6 +5,16 @@ namespace SimpleECS
 	public class World
 	{
 		private HashSet<Entity> entities = new HashSet<Entity>();
+
+		public Entity CreateEntity()
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void DestroyEntity(Entity entity)
+		{
+			throw new System.NotImplementedException();
+		}
 	}
 }
 
