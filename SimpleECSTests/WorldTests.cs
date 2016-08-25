9539a4cbbbe928064511e9130f092fee329e2c2d
Murat Gurel <murat@gram.gs>
2016-08-25 13:59:48 +0300

    Adds world tests

--- SimpleECSTests/WorldTests.cs
+++ SimpleECSTests/WorldTests.cs
@@ -1,11 +1,52 @@
 ﻿using NUnit.Framework;
 
-namespace SimpleECSTests
+namespace SimpleECS.Test
 {
 	[TestFixture]
 	public class WorldTests
 	{
-		
+		private World world;
+
+		[SetUp]
+		public void SetUp()
+		{
+			world = new World();
+		}
+
+		[Test]
+		public void ShouldCreateNewEntityWithZeroComponents()
+		{
+			Entity newEntity = world.CreateEntity();
+			Assert.AreEqual(newEntity.componentCount, 0);
+		}
+
+		[Test]
+		public void ShouldAddNewEntityToWorldAfterCreate()
+		{
+			world.CreateEntity();
+			Assert.AreEqual(world.entityCount, 1);
+		}
+
+		[Test]
+		public void ShouldClearAllComponentsOnDestroy()
+		{
+			Entity newEntity = world.CreateEntity();
+			newEntity.AddComponent<EmptyComponent>();
+			world.DestroyEntity(newEntity);
+
+			Assert.AreEqual(newEntity.componentCount, 0);
+		}
+
+		[Test]
+		public void ShouldRemoveEntityFromWorldAfterDestroy()
+		{
+			Entity newEntity = world.CreateEntity();
+			world.DestroyEntity(newEntity);
+
+			Assert.AreEqual(world.entityCount, 0);
+		}
+
+		private class EmptyComponent : IComponent {}
 	}
 }
 
