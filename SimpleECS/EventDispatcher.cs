679930735c24809ab13afb6622e4c0271b557ee2
Murat Gurel <mrtgurel@gmail.com>
2016-09-04 23:35:21 +0300

    Adds event queue and dispatcher

---
+++ SimpleECS/EventDispatcher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleECS
+{
+    public class EventDispatcher
+    {
+        private static readonly Dictionary<string, List<EventQueue>> queues = new Dictionary<string, List<EventQueue>>();
+
+        public static void Dispatch(string eventName, object data)
+        {
+            foreach (var queue in queues[eventName])
+            {
+                queue.OnEvent(data);
+            }
+        }
+
+        internal static void Register(string eventName, EventQueue queue)
+        {
+            if (!queues.ContainsKey(eventName))
+            {
+                queues.Add(eventName, new List<EventQueue>());
+            }
+
+            queues[eventName].Add(queue);
+        }
+
+        internal static void Unregister(string eventName, EventQueue queue)
+        {
+            queues[eventName].Remove(queue);
+        }
+    }
+}
---
+++ SimpleECS/EventQueue.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleECS
+{
+    public sealed class EventQueue
+    {
+        private readonly Queue<object> events = new Queue<object>();
+
+        public int pending
+        {
+            get
+            {
+                return events.Count;
+            }
+        }
+
+        public EventQueue(string eventName)
+        {
+            EventDispatcher.Register(eventName, this);
+        }
+
+        public object Get()
+        {
+            return events.Dequeue();
+        }
+
+        public object[] GetAll()
+        {
+            return events.ToArray();
+        }
+
+        internal void OnEvent(object data)
+        {
+            events.Enqueue(data);
+        }
+    }
+}
