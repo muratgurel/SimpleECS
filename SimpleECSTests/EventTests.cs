e9b3b6c660e860f014865db93c7ffe7073254a95
Murat Gurel <mrtgurel@gmail.com>
2016-09-04 23:41:31 +0300

    Adds an example system using event queues

---
+++ SimpleECSTests/EventTests.cs
@@ -0,0 +1,38 @@
+﻿
+namespace SimpleECS.Test
+{
+    public class EventExample
+    {
+        private class ExampleSystem : System
+        {
+            private EventQueue attackQueue = new EventQueue("attack");
+            private EventQueue blockQueue = new EventQueue("block");
+
+            public override IPredicate<Entity> entityPredicate
+            {
+                get
+                {
+                    return EntityPredicate.New;
+                }
+            }
+
+            public void Update(float deltaTime)
+            {
+                foreach (var attack in attackQueue.GetAll())
+                {
+                    // Do something
+                }
+
+                foreach (var block in blockQueue.GetAll())
+                {
+                    // Do something else
+                }
+
+                foreach (var entity in GetEntities())
+                {
+                    // Now do something with your entities
+                }
+            }
+        }
+    }
+}
