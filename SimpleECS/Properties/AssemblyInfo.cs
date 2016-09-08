1049a18879843d59ed402575d4969cffd3197320
Murat Gurel <murat@gram.gs>
2016-09-08 13:54:45 +0300

    Updates project settings

--- SimpleECS/Properties/AssemblyInfo.cs
+++ SimpleECS/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("SimpleECS")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Gram Games")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Murat Gurel")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
