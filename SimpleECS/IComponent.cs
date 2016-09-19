f73042519845caf8966e40011026bbb41064c4bb
Murat Gurel <murat@gram.gs>
2016-09-19 15:11:22 +0300

    Adds comment to IComponent

--- SimpleECS/IComponent.cs
+++ SimpleECS/IComponent.cs
@@ -1,6 +1,11 @@
 ﻿
 namespace SimpleECS
 {
+	/// <summary>
+	/// All components should implement IComponent interface,
+	/// so that they can be added or removed to/from entities.
+	/// You don't have to do anything else.
+	/// </summary>
 	public interface IComponent
 	{
 	}
