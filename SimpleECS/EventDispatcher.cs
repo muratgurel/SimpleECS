cd353f6d44359ce08239421bd6020d4e18328808
Murat Gurel <murat@gram.gs>
2016-09-19 15:09:43 +0300

    Adds comments to event system classes

--- SimpleECS/EventDispatcher.cs
+++ SimpleECS/EventDispatcher.cs
@@ -2,10 +2,20 @@
 
 namespace SimpleECS
 {
+	/// <summary>
+	/// Use EventDispatcher.Dispatch(eventName, data) to dispatch
+	/// event to event queues.
+	/// </summary>
     public class EventDispatcher
     {
         private static readonly Dictionary<string, List<EventQueue>> queues = new Dictionary<string, List<EventQueue>>();
 
+		/// <summary>
+		/// Dispatch the specified event with an optional data. The event
+		/// will be queued in EventQueues that are listening to this event.
+		/// </summary>
+		/// <param name="eventName">Event name</param>
+		/// <param name="data">Optional data object. Can be null.</param>
         public static void Dispatch(string eventName, object data)
         {
 			if (!queues.ContainsKey(eventName))
@@ -19,6 +29,9 @@ namespace SimpleECS
             }
         }
 
+		/// <summary>
+		/// EventQueues should be registered using their StartListening method
+		/// </summary>
         internal static void Register(string eventName, EventQueue queue)
         {
             if (!queues.ContainsKey(eventName))
@@ -29,6 +42,9 @@ namespace SimpleECS
             queues[eventName].Add(queue);
         }
 
+		/// <summary>
+		/// EventQueues should be unregistered using their StopListening method
+		/// </summary>
         internal static void Unregister(string eventName, EventQueue queue)
         {
             queues[eventName].Remove(queue);
--- SimpleECS/EventQueue.cs
+++ SimpleECS/EventQueue.cs
@@ -2,6 +2,12 @@
 
 namespace SimpleECS
 {
+	/// <summary>
+	/// Get a new EventQueue using the constructor
+	/// new EventQueue(eventName) and register it to receive
+	/// event using StartListening method. To stop receiving event,
+	/// call the StopListening method.
+	/// </summary>
     public sealed class EventQueue
     {
         private readonly Queue<object> events = new Queue<object>();
@@ -27,23 +33,40 @@ namespace SimpleECS
 			this.eventName = eventName;
         }
 
+		/// <summary>
+		/// Registers to dispatcher. The queue will
+		/// start receiving events.
+		/// </summary>
 		public void StartListening()
 		{
 			EventDispatcher.Register(eventName, this);
 			isRegistered = true;
 		}
 
+		/// <summary>
+		/// Unregisters from the dispatcher. The queue will
+		/// not receive any events.
+		/// </summary>
 		public void StopListening()
 		{
 			EventDispatcher.Unregister(eventName, this);
 			isRegistered = false;
 		}
 
+		/// <summary>
+		/// Dequeue the next event. Removes the event from
+		/// the queue.
+		/// </summary>
         public object Get()
         {
             return events.Dequeue();
         }
 
+		/// <summary>
+		/// Dequeue all events. Queue is emptied after
+		/// this.
+		/// </summary>
+		/// <returns>The all.</returns>
         public object[] GetAll()
         {
             object[] allEvents = events.ToArray();
